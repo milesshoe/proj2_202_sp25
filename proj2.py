f7193a4ca9d169def1a41105a40443dc9282eb45
milesshoe <mjschuli@calpoly.edu>
2026-05-02 12:48:08 -0700

    commit

--- proj2.py
+++ proj2.py
@@ -1,6 +1,7 @@
 import csv
 import math
 from dataclasses import dataclass
+from reprlib import recursive_repr
 from typing import *
 import sys
 sys.setrecursionlimit(10_000)
@@ -12,6 +13,7 @@ class Row:
     country: str
     year: int
     electricity_and_heat_co2_emissions: float|None
+    electricity_and_heat_co2_emissions_per_capita: float|None
     energy_co2_emissions: float|None
     energy_co2_emissions_per_capita: float|None
     total_co2_emissions_excluding_lucf: float|None
@@ -27,24 +29,32 @@ class Node:
 # Task 2
 def parse_row(fields: list[str]) -> Row:
         new_row = Row(country = fields[0],
-                      year = int(fields[1] if fields[1] is not "" else 0),
-                      electricity_and_heat_co2_emissions = float(fields[2] if fields[2] is not "" else 0),
-                      energy_co2_emissions = float(fields[3] if fields[3] is not "" else 0),
-                      energy_co2_emissions_per_capita = float(fields[4] if fields[4] is not "" else 0),
-                      total_co2_emissions_excluding_lucf = float(fields[5] if fields[5] is not "" else 0),
-                      total_co2_emissions_excluding_lucf_per_capita = float(fields[6] if fields[6] is not "" else 0))
-        print(new_row)
+                      year = int(fields[1]) if fields[1] != "" else None,
+                      electricity_and_heat_co2_emissions = float(fields[2]) if fields[2] != "" else None,
+                      electricity_and_heat_co2_emissions_per_capita = float(fields[3]) if fields[3] != "" else None,
+                      energy_co2_emissions = float(fields[4]) if fields[4] != "" else None,
+                      energy_co2_emissions_per_capita = float(fields[5]) if fields[5] != "" else None,
+                      total_co2_emissions_excluding_lucf = float(fields[6]) if fields[6] != "" else None,
+                      total_co2_emissions_excluding_lucf_per_capita = float(fields[7]) if fields[7] != "" else None)
+        return new_row
+
+def recursive_func(objs: list[Row])-> Optional[Node]:
+    if objs == []:
+        return None
+    return Node(objs[0],recursive_func(objs[1:]))
+
+
+
 
 def read_csv_lines(filename: str) -> Optional[Node]:
     with open(filename, newline='') as csvfile:
         all_rows = csv.reader(csvfile, delimiter=',')
-        header = next(all_rows)
+        next(all_rows)
+        list_of_rows=[]
         for row in all_rows:
-            row_class = parse_row(row)
-
-
-
-
+              list_of_rows.append(parse_row(row))
+        linked_list= recursive_func(list_of_rows)
+        return linked_list
 
 
 read_csv_lines("some-ghg-emissions.csv")
